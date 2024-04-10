3df259c6631cfb55dfa9d7b79ff3133dba06bd6e
lolodev <horaciodev1706@gmail.com>
2024-04-10 17:45:23 -0300

    Navbar

--- 11-navbar/setup/src/data.js
+++ 11-navbar/setup/src/data.js
@@ -1,52 +1,52 @@
-import React from 'react'
-import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import React from 'react';
+import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa';
 export const links = [
-  {
-    id: 1,
-    url: '/',
-    text: 'random',
-  },
-  {
-    id: 2,
-    url: '/about',
-    text: 'about',
-  },
-  {
-    id: 3,
-    url: '/projects',
-    text: 'projects',
-  },
-  {
-    id: 4,
-    url: '/contact',
-    text: 'contact',
-  },
-  {
-    id: 5,
-    url: '/profile',
-    text: 'profile',
-  },
-]
+	{
+		id: 1,
+		url: '/',
+		text: 'inicio',
+	},
+	{
+		id: 2,
+		url: '/about',
+		text: 'quienes somos',
+	},
+	{
+		id: 3,
+		url: '/promos',
+		text: 'promos',
+	},
+	{
+		id: 4,
+		url: '/contact',
+		text: 'contacto',
+	},
+	{
+		id: 5,
+		url: '/products',
+		text: 'productos',
+	},
+];
 
 export const social = [
-  {
-    id: 1,
-    url: 'https://www.twitter.com',
-    icon: <FaFacebook />,
-  },
-  {
-    id: 2,
-    url: 'https://www.twitter.com',
-    icon: <FaTwitter />,
-  },
-  {
-    id: 3,
-    url: 'https://www.twitter.com',
-    icon: <FaLinkedin />,
-  },
-  {
-    id: 4,
-    url: 'https://www.twitter.com',
-    icon: <FaBehance />,
-  },
-]
+	{
+		id: 1,
+		url: 'https://www.twitter.com',
+		icon: <FaFacebook />,
+	},
+	{
+		id: 2,
+		url: 'https://www.twitter.com',
+		icon: <FaTwitter />,
+	},
+	{
+		id: 3,
+		url: 'https://www.twitter.com',
+		icon: <FaLinkedin />,
+	},
+	{
+		id: 4,
+		url: 'https://www.twitter.com',
+		icon: <FaBehance />,
+	},
+];
