33c1293ca21a671affcd96a82d4ecac1468f9179
lolodev <horaciodev1706@gmail.com>
2024-04-12 21:50:50 -0300

    Cocktails

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+	return (
+		<section className='section about-section'>
+			<h1 className='section-title'>about us</h1>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat atque
+				veniam omnis assumenda suscipit! Neque, similique! Labore quasi dolorem
+				mollitia, ipsam aliquid fugiat obcaecati, omnis possimus dolore ratione
+				molestias eum? Veritatis neque inventore recusandae quisquam, iure
+				ullam. Nisi, ab totam.
+			</p>
+		</section>
+	);
+};
 
-export default About
+export default About;
